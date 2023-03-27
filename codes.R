8a541eb0301a529c02e0b1703ed2f2978b7ef342
kytusla908 <jordiren99@gmail.com>
2023-03-27 14:08:52 +0200

    Added cor based variable selection

---
+++ codes.R
@@ -0,0 +1,25 @@
+# Input archives reading
+data_chembl <- read.csv("data.csv",sep = ";",dec = ".")
+data <- read.csv("CHEMBL4372_Desc.csv",sep = ",",dec = ".")
+variables <- data[ ,9:127]  # Df containing only the descriptors
+
+# DATA PREPARATION ============================
+# Correlation based Feature Selection ==========
+cor_matrix <- cor(variables)  # Calculation cor matrix
+cor_matrix[!lower.tri(cor_matrix)] <- 0  # Erasing half of the mirror matrix
+
+# Eliminating highly correlated variables
+variables_0.8 <- variables[, !apply(cor_matrix, 2,
+                                    function(x) any(abs(x) > 0.8, na.rm = TRUE))]
+variables_0.85 <- variables[, !apply(cor_matrix, 2,
+                                    function(x) any(abs(x) > 0.85, na.rm = TRUE))]
+variables_0.9 <- variables[, !apply(cor_matrix, 2,
+                                    function(x) any(abs(x) > 0.9, na.rm = TRUE))]
+cat("Nº Variables usando 0.8 como punto de corte: ", ncol(variables_0.8),
+      "\nNº Variables usando 0.85 como punto de corte: ", ncol(variables_0.85),
+      "\nNº Variables usando 0.9 como punto de corte: ", ncol(variables_0.9))
+
+# Variance based Feature Selection ==========
+
+
+
