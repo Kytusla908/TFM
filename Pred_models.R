06167a822e5c6eae3d15efa74511a6c9140cdd88
kytusla908 <jordiren99@gmail.com>
2023-04-17 15:55:12 +0200

    Added Random Forest model

--- Pred_models.R
+++ Pred_models.R
@@ -17,33 +17,42 @@ ctrl <- trainControl(method = "repeatedcv",
 set.seed(12345)
 grid <- expand.grid(k = c(1,3,5,7,15,21,27,35))
 kNN_model <- train(label ~ ., data = norm_data, method = "knn",
-                   tuneGrid = grid, trControl = crtl)
+                   tuneGrid = grid, trControl = ctrl)
 kNN_model
 plot(kNN_model)
 
 # Predictions
 kNN_model_pred <- predict(kNN_model, test[-1])
 table(kNN_model_pred == test[[1]])
-confusionMatrix(kNN_model_pred, test[[1]], 
-                positive = "1")
+values_kNN_mod <- confusionMatrix(kNN_model_pred, test[[1]],
+                                  positive = "1")
 
 
 # Naive Bayes ============
 # Automated paramenter tuning
 set.seed(12345)
 grid <- expand.grid(usekernel = c(FALSE,TRUE),
+                    laplace = seq(0,1.5, length = 4),
+                    adjust = seq(0,1.5, length = 4))
+NB_model <- train(label ~ ., data = norm_data, method = "naive_bayes",
+                  tuneGrid = grid, trControl = ctrl)
+NB_model
+plot(NB_model)
+
+set.seed(12345)
+grid <- expand.grid(usekernel = c(TRUE),
                     laplace = seq(0,1.5, length = 15),
                     adjust = seq(0,1.5, length = 15))
 NB_model <- train(label ~ ., data = norm_data, method = "naive_bayes",
-                  tuneGrid = grid, trControl = crtl)
+                  tuneGrid = grid, trControl = ctrl)
 NB_model
 plot(NB_model)
 
 # Predictions
 NaiveBayes_pred <- predict(NB_model, test[-1])
 table(NaiveBayes_pred == test[[1]])
-confusionMatrix(NaiveBayes_pred, test[[1]], 
-                positive = "1")
+values_NB_mod <- confusionMatrix(NaiveBayes_pred, test[[1]], 
+                                  positive = "1")
 
 
 # Support Vector Machine ===========
@@ -76,10 +85,19 @@ SVM <- data.frame(model = c("Linear model","RBF model", "Polinomial model"),
 SVM %>% arrange(accuracy)
 
 
-# Random forests ==========
+# Random forest ==========
+set.seed(12345)
+p <- round(sqrt(ncol(norm_data)))
+grid <- expand.grid(mtry = seq(p,p+2*p))
+RF_model <- train(label ~ ., data = norm_data, method = "rf",
+                  tuneGrid = grid, trControl = ctrl)
+RF_model
+plot(RF_model)
 
-
-
-
+# Predictions
+RandomForest_pred <- predict(RF_model, test[-1])
+table(RandomForest_pred == test[[1]])
+values_NB_mod <- confusionMatrix(RandomForest_pred, test[[1]], 
+                                 positive = "1")
 
 
