d8e2a82512b776953e39f1ec8d56b6b9d23a32e1
kytusla908 <jordiren99@gmail.com>
2023-04-20 12:24:22 +0200

    fixed minor errors

--- Pred_models.R
+++ Pred_models.R
@@ -16,7 +16,7 @@ ctrl <- trainControl(method = "repeatedcv",
 # Automated paramenter tuning
 grid <- expand.grid(k = c(1,3,5,7,15,21,27,35))
 set.seed(12345)
-kNN_model <- train(label ~ ., data = norm_data, method = "knn",
+kNN_model <- train(label ~ ., data = norm_data[-1], method = "knn",
                    tuneGrid = grid, trControl = ctrl)
 kNN_model
 
@@ -29,9 +29,9 @@ kNN_plot <- ggplot(kNN_model) +
 kNN_plot
 
 # Predictions
-kNN_model_pred <- predict(kNN_model, test[-1])
-table(kNN_model_pred == test[[1]])
-values_kNN_mod <- confusionMatrix(kNN_model_pred, test[[1]],
+kNN_model_pred <- predict(kNN_model, test[-(1:2)])
+table(kNN_model_pred == test[[2]])
+values_kNN_mod <- confusionMatrix(kNN_model_pred, test[[2]],
                                   positive = "1")
 values_kNN_mod
 
@@ -42,7 +42,7 @@ grid <- expand.grid(usekernel = c(FALSE,TRUE),
                     laplace = seq(0,1.5, length = 5),
                     adjust = seq(0,1.5, length = 5))
 set.seed(12345)
-NB_model1 <- train(label ~ ., data = norm_data, method = "naive_bayes",
+NB_model1 <- train(label ~ ., data = norm_data[-1], method = "naive_bayes",
                   tuneGrid = grid, trControl = ctrl)
 NB_model1
 
@@ -56,7 +56,7 @@ grid <- expand.grid(usekernel = c(TRUE),
                     laplace = 0,
                     adjust = seq(0,1.5, length = 15))
 set.seed(12345)
-NB_model <- train(label ~ ., data = norm_data, method = "naive_bayes",
+NB_model <- train(label ~ ., data = norm_data[-1], method = "naive_bayes",
                   tuneGrid = grid, trControl = ctrl)
 NB_model
 
@@ -70,33 +70,33 @@ NB_plot <- ggplot(NB_model) +
 NB_plot
 
 # Predictions
-NaiveBayes_pred <- predict(NB_model, test[-1])
-table(NaiveBayes_pred == test[[1]])
-values_NB_mod <- confusionMatrix(NaiveBayes_pred, test[[1]], 
+NaiveBayes_pred <- predict(NB_model, test[-(1:2)])
+table(NaiveBayes_pred == test[[2]])
+values_NB_mod <- confusionMatrix(NaiveBayes_pred, test[[2]], 
                                   positive = "1")
 values_NB_mod
 
 
 # Support Vector Machine ===========
 # SVM linear classifier
-svm_lin_model <- ksvm(label ~ ., data = norm_data, kernel = "vanilladot")
-svm_lin_pred <- predict(svm_lin_model, test[-1])
-table(svm_lin_pred == test[[1]])
-values_lin_mod <- confusionMatrix(svm_lin_pred, test[[1]], 
+svm_lin_model <- ksvm(label ~ ., data = norm_data[-1], kernel = "vanilladot")
+svm_lin_pred <- predict(svm_lin_model, test[-(1:2)])
+table(svm_lin_pred == test[[2]])
+values_lin_mod <- confusionMatrix(svm_lin_pred, test[[2]], 
                 positive = "1")
 
 # SVM with Radial Basis Function Kernel
-svm_rbf_model <- ksvm(label ~ ., data = norm_data, kernel = "rbfdot")
-svm_rbf_pred <- predict(svm_rbf_model, test[-1])
-table(svm_rbf_pred == test[[1]])
-values_rbf_mod <- confusionMatrix(svm_rbf_pred, test[[1]], 
+svm_rbf_model <- ksvm(label ~ ., data = norm_data[-1], kernel = "rbfdot")
+svm_rbf_pred <- predict(svm_rbf_model, test[-(1:2)])
+table(svm_rbf_pred == test[[2]])
+values_rbf_mod <- confusionMatrix(svm_rbf_pred, test[[2]], 
                 positive = "1")
 
 # SVM with Polynomial Kernel
-svm_poly_model <- ksvm(label ~ ., data = norm_data, kernel = "polydot")
-svm_poly_pred <- predict(svm_poly_model, test[-1])
-table(svm_poly_pred == test[[1]])
-values_poly_mod <- confusionMatrix(svm_poly_pred, test[[1]], 
+svm_poly_model <- ksvm(label ~ ., data = norm_data[-1], kernel = "polydot")
+svm_poly_pred <- predict(svm_poly_model, test[-(1:2)])
+table(svm_poly_pred == test[[2]])
+values_poly_mod <- confusionMatrix(svm_poly_pred, test[[2]], 
                 positive = "1")
 
 # Summary of results
@@ -117,7 +117,7 @@ SVM_plot
 p <- round(sqrt(ncol(norm_data)))
 grid <- expand.grid(mtry = seq(p,p+2*p))
 set.seed(12345)
-RF_model <- train(label ~ ., data = norm_data, method = "rf",
+RF_model <- train(label ~ ., data = norm_data[-1], method = "rf",
                   tuneGrid = grid, trControl = ctrl)
 RF_model
 
@@ -129,9 +129,9 @@ RF_plot <- ggplot(RF_model) +
 RF_plot
 
 # Predictions
-RandomForest_pred <- predict(RF_model, test[-1])
-table(RandomForest_pred == test[[1]])
-values_RF_mod <- confusionMatrix(RandomForest_pred, test[[1]], 
+RandomForest_pred <- predict(RF_model, test[-(1:2)])
+table(RandomForest_pred == test[[2]])
+values_RF_mod <- confusionMatrix(RandomForest_pred, test[[2]], 
                                  positive = "1")
 values_RF_mod
 
